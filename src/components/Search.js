4c10431ca1bfd3bf71c0bdeef8bc05a84c5a3306
Olumide Victoria <olumideronke@gmail.com>
2022-12-03 21:23:22 -0800

    ANother

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,8 +4,8 @@ import styled from 'styled-components'
 const Search = () => {
   return (
     <SearchStyle className='flex justify-between mx-[3rem]'>
-        <input type="text"className='border-[3px] border-[gray] w-[75%]' />
-        <button className='bg-[#67111c] py-[.5rem] px-[1rem] text-[#fff]'>Search</button>
+        <input type="text"className='border-[2px] border-[gray] w-[75%] font-[1rem]' />
+        <button className='bg-[#67111c] py-[.5rem] px-[1rem] text-[#fff] font-[1rem]'>Search</button>
     </SearchStyle>
   )
 }
